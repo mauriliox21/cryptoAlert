fffcb7749eb290a51b9b534ea44ffa1a21aee85b
mauriliox21 <mauriliox21@gmail.com>
2024-04-16 10:48:20 -0300

    fix: update time of expiration token JWT

--- src/main/java/com/maurlox21/cryptoalert/jwt/JwtUtils.java
+++ src/main/java/com/maurlox21/cryptoalert/jwt/JwtUtils.java
@@ -20,8 +20,8 @@ public class JwtUtils {
     public static final String SECRET_KEY = "0123456789-0123456789-0123456789";
     public static final String JWT_BEARER = "Bearer ";
     public static final String JWT_AUTHORIZATION = "Authorization";
-    public static final long EXPIRE_DAYS = 0;
-    public static final long EXPIRE_HOURS = 1;
+    public static final long EXPIRE_DAYS = 30;
+    public static final long EXPIRE_HOURS = 0;
     public static final long EXPIRE_MINUTES = 0;
 
     public static JwtToken createToken(String email, String role) {
